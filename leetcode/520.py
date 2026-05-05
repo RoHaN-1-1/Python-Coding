d472d9111998554211bb6875ddeaaf69e1e3a06b
Rohan K <rohan.kandepaneni@gmail.com>
2026-05-05 08:58:45 -0700

    added question 520

---
+++ leetcode/520.py
@@ -0,0 +1,11 @@
+# class Solution:
+def detectCapitalUse(word):
+    if word.isupper():
+        return True
+    if word.islower():
+        return True
+    if word[0].isupper() and word[1:].islower():
+        return True
+    return False
+word = "Google"
+print(detectCapitalUse(word))
